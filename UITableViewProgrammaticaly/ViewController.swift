7cd90a6bacaf7985df9810031abbb172257de0d9
Wesley <whsieh1996@gmail.com>
2020-06-29 22:29:29 -0700

    Initial Commit

---
+++ UITableViewProgrammaticaly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewProgrammaticaly
+//
+//  Created by Wesley on 6/29/20.
+//  Copyright © 2020 Wesley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
